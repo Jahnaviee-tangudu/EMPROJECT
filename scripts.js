b647d664619b0da6e1217de122d6ce382560053e
Jahnaviee-tangudu <jahnaviee22@gmail.com>
2024-06-15 20:06:50 +0530

    ADDED 16 FILES

---
+++ scripts.js
@@ -0,0 +1,36 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+    let query = document.getElementById('searchInput').value.toLowerCase();
+    handleSearch(query);
+  });
+  
+  document.getElementById('searchInput').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      document.getElementById('searchButton').click();
+    }
+  });
+  function handleSearch(query) {
+    if (query.includes('horror')) {
+      window.location.href = 'horror.html';
+      console.log("called horror");
+    } else if (query.includes('comedy')) {
+      window.location.href = 'comedy movies.html';
+    } else if (query.includes('action')) {
+      window.location.href = 'action movies.html';
+    } 
+    else if (query.includes('latest')) {
+        window.location.href = 'latest movies.html';
+      } 
+      else if (query.includes('upcoming')) {
+        window.location.href = 'upcoming movies.html';
+      } 
+      else if (query.includes('romantic')) {
+        window.location.href = 'romantic movies.html';
+      } 
+    
+    else {
+      alert('No matching page found.');
+    }
+  }
+  
+ 
